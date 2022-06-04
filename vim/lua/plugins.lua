044d166f7693c529593f9f6841639d00a8894936
atomer <latomu@qq.com>
2022-06-04 08:29:26 +0800

    add dap

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -20,8 +20,15 @@ packer.startup(function()
     'nvim-telescope/telescope.nvim',
     requires = {{'nvim-lua/plenary.nvim'}}
   }
+
   use {"akinsho/toggleterm.nvim", tag = 'v1.*', config = function()
     require("toggleterm").setup()
   end}
+
+  use "ravenxrz/DAPInstall.nvim"
+  use "ravenxrz/nvim-dap"
+  use "theHamsta/nvim-dap-virtual-text"
+  use "rcarriga/nvim-dap-ui"
+  use "nvim-telescope/telescope-dap.nvim"
 end)
 
