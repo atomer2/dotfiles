07ead8f6a2b1476be131c7b5e3f967eab635f04b
atomer <latomu@qq.com>
2023-08-19 09:14:03 +0800

    opacity

--- vim/lua/user/colorscheme.lua
+++ vim/lua/user/colorscheme.lua
@@ -1,1 +1,2 @@
 vim.cmd("colorscheme onedark")
+vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
--- vim/lua/user/options.lua
+++ vim/lua/user/options.lua
@@ -3,3 +3,4 @@ vim.o.smartindent = true
 vim.o.autoindent = true
 vim.o.number = true
 vim.o.shiftwidth = 4
+
