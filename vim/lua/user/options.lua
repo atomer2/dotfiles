5dca91a3c48e7e83f7ad1625f5929182610e3890
atomer <latomu@qq.com>
2023-08-24 11:29:33 +0800

    save

--- vim/lua/user/options.lua
+++ vim/lua/user/options.lua
@@ -3,7 +3,6 @@ vim.o.smartindent = true
 vim.o.autoindent = true
 vim.o.number = true
 vim.o.shiftwidth = 4
-vim.o.shiftwidth = 2
 vim.o.incsearch = true
 vim.o.wrap = true
 vim.o.autoread = true
--- vim/lua/user/telescope.lua
+++ vim/lua/user/telescope.lua
@@ -2,3 +2,4 @@ vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {noremap = tr
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>", {noremap = true})
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", {noremap = true})
 vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", {noremap = true})
+vim.keymap.set("n", "<leader>lg", "<cmd>Telescope live_grep<cr>", {noremap = true})
